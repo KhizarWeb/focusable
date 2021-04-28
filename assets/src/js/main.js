f9a34fa10cb9151f01f62af64f46d57b63f949f9
Khizar <khizarhasan927@gmail.com>
2021-04-28 21:22:40 +0530

    make focus ring color based on elements background color

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -3,15 +3,12 @@ class Focusable {
     this.focusSource = ally.style.focusSource();
     this.focusableElements = ally.query.focusable();
     this.activeElement = ally.event.activeElement();
-    this.activeElementBoundingRect = null;
+    this.activeElementBoundingRect = {};
     this.focusRingElement = document.createElement("span");
     this.focusRingElement.classList.add("focusable__ring");
-    this.outlineColor = this.invertColor(
-      window
-        .getComputedStyle(document.body, null)
-        .getPropertyValue("background-color"),
-      true
-    );
+    this.outlineColor = window
+      .getComputedStyle(document.body, null)
+      .getPropertyValue("background-color");
 
     document.body.insertAdjacentElement("beforeend", this.focusRingElement);
 
@@ -32,8 +29,6 @@ class Focusable {
         if (this.focusSource.used("key") === false) {
           return;
         }
-        // event.detail.focus: element that received focus
-        // event.detail.blur: element that lost focus
 
         this.activeElement = event.detail.focus;
 
@@ -57,22 +52,24 @@ class Focusable {
 
     this.activeElementBoundingRect = this.activeElement.getBoundingClientRect();
 
-    let outlineColor = this.outlineColor;
-    let activeOutlineColor = window
-      .getComputedStyle(this.activeElement, null)
-      .getPropertyValue("outline-color");
+    this.getBackgroundColor(this.activeElement);
 
-    if (activeOutlineColor === "rgb(0, 0, 0)") {
-      outlineColor = this.outlineColor;
-    } else {
-      outlineColor = activeOutlineColor;
-    }
+    // let outlineColor = this.outlineColor;
+    // let activeOutlineColor = window
+    //   .getComputedStyle(this.activeElement, null)
+    //   .getPropertyValue("outline-color");
+
+    // if (activeOutlineColor === "rgb(0, 0, 0)") {
+    //   outlineColor = this.outlineColor;
+    // } else {
+    //   outlineColor = activeOutlineColor;
+    // }
 
     const values = {
       transform: `translate(${this.activeElementBoundingRect.left}px, ${this.activeElementBoundingRect.top}px)`,
       width: this.activeElementBoundingRect.width + "px",
       height: this.activeElementBoundingRect.height + "px",
-      outlineColor: outlineColor,
+      outlineColor: this.outlineColor,
     };
 
     for (const property in values) {
@@ -83,18 +80,28 @@ class Focusable {
     }
   }
 
+  getBackgroundColor(element) {
+    let backgroundColor = window
+      .getComputedStyle(element, null)
+      .getPropertyValue("background-color");
+
+    if (backgroundColor === "rgba(0, 0, 0, 0)" && element !== document.body) {
+      this.getBackgroundColor(element.parentElement);
+    } else {
+      this.outlineColor = backgroundColor;
+      return;
+    }
+  }
+
   invertColor(rgb) {
     let RGB = rgb
       .substring(5, rgb.length - 1)
       .replace(/ /g, "")
       .split(",");
 
-    // invert color components
-    let r = RGB[0],
-      g = RGB[1],
-      b = RGB[2];
-
-    return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "#000000" : "#FFFFFF";
+    return RGB[0] * 0.299 + RGB[1] * 0.587 + RGB[2] * 0.114 > 125
+      ? "#000000"
+      : "#FFFFFF";
   }
 
   isElement(element) {
